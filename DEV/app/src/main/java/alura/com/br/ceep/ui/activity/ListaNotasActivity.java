682736cea05a12b4fa30450828f045f09ca0e2c7
Romero <40298952+lucianoromero@users.noreply.github.com>
2021-08-31 11:30:02 -0300

    Criando classes para testar a ListView

--- DEV/app/src/main/java/alura/com/br/ceep/ui/activity/ListaNotasActivity.java
+++ DEV/app/src/main/java/alura/com/br/ceep/ui/activity/ListaNotasActivity.java
@@ -3,8 +3,14 @@ package alura.com.br.ceep.ui.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.List;
 
 import alura.com.br.ceep.R;
+import alura.com.br.ceep.dao.NotaDAO;
+import alura.com.br.ceep.model.Nota;
+import alura.com.br.ceep.ui.adapter.ListaNotasAdapter;
 
 public class ListaNotasActivity extends AppCompatActivity {
 
@@ -12,5 +18,13 @@ public class ListaNotasActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_notas);
+        ListView listaNotas = findViewById(R.id.listView);
+
+        NotaDAO dao = new NotaDAO();
+        dao.insere(new Nota("Primeira nota",
+                "Primeira descrição"));
+        List<Nota> todasNotas = dao.todos();
+
+        listaNotas.setAdapter(new ListaNotasAdapter(this, todasNotas));
     }
 }
